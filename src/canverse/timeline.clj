78ba5b132ab401be8b28c89a525ebf45b504ecd5
akbiggs <alexanderkbiggs@gmail.com>
2014-02-26 01:11:04 -0500

    Draw the timeline properly, fix occasional massive lag burst.

--- src/canverse/timeline.clj
+++ src/canverse/timeline.clj
@@ -1,5 +1,9 @@
 (ns canverse.timeline
-  (:require [quil.core :as q]))
+  (:require [canverse.helpers :as helpers]
+            [canverse.square :as square]
+            [canverse.synths :as synths]
+            [overtone.live :as o]
+            [quil.core :as q]))
 
 (defn create [history-length width]
   "Creates a new timeline retaining history-length
@@ -8,29 +12,45 @@
    :history-length history-length
    :width width})
 
-(defn update [timeline]
+(defn add-note-from-node [node timeline]
+  (assoc timeline :history
+    (let [{freq :freq amp :amp} (o/node-get-controls node [:freq :amp])
+          new-x (:width timeline)
+          relative-frequency (helpers/relative-in-scale freq square/scale)
+          new-y (- 395 (* 45 relative-frequency))
+          new-note {:x new-x :y new-y :size [1 2] :amp amp :relative-freq relative-frequency}
+          history (:history timeline)]
+      (conj history new-note))))
+
+(defn add-notes-from-nodes [nodes timeline]
+  (reduce #(add-note-from-node %2 %1) timeline
+          (filter o/node-live? nodes)))
+
+(defn update [elapsed-time timeline]
   (assoc timeline
     :history
-    (let [delta @(q/state :time-delta)
-          delta-ratio (/ delta (:history-length timeline))
+    (let [movement-ratio (/ elapsed-time (:history-length timeline))
           width (:width timeline)
-          movement (* delta-ratio width)
+          movement (* movement-ratio width)
           history (:history timeline)]
       (vec (for [note history]
         (assoc note :x (- (:x note) movement)))))))
 
-(defn add-note [relative-pitch timeline]
-  (assoc timeline :history
-    (let [new-x (:width timeline)
-          new-y (- 400 (* 10 relative-pitch))
-          new-note {:x new-x :y new-y :size [1 10]}
-          history (:history timeline)]
-      (concat history new-note))))
-
 (defn draw [timeline]
-  (q/fill 255)
+  (q/push-style)
+
   (doseq [note (:history timeline)]
     (let [x (:x note)
           y (:y note)
+          alpha (* 255 (:amp note))
+          relative-freq (:relative-freq note)
+          color (q/lerp-color (apply q/color (second square/COLUMN_COLORS))
+                              (apply q/color (last square/COLUMN_COLORS))
+                              relative-freq)
           [w h] (:size note)]
-      (q/rect x y w h))))
+
+      (q/no-stroke)
+      (q/fill color alpha)
+      (q/rect x y w h)))
+
+  (q/pop-style))
